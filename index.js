b63649fb3bfe9611da36c4e84dff7f07843f642a
Shashank Racherla <shashank.racherla@gmail.com>
2018-06-24 16:38:26 -0700

    plan to remove mongodb

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ require('./models/questions');
 require('./models/answers');
 const app = express();
 
-mongoose.connect('mongodb://tester:password123@ds018558.mlab.com:18558/codeup-trial');
-const Question = mongoose.model('questions');
-const Answer = mongoose.model('answers');
-app.get('/', (req,res) => {
-    res.send({hello: 'world'});
-    new Question({
-        question: "Ayyy",
-        correctAnswerId: 1
-    }).save();
+// mongoose.connect('mongodb://tester:password123@ds018558.mlab.com:18558/codeup-trial');
+// const Question = mongoose.model('questions');
+// const Answer = mongoose.model('answers');
+// app.get('/', (req,res) => {
+//     res.send({hello: 'world'});
+//     new Question({
+//         question: "Ayyy",
+//         correctAnswerId: 1
+//     }).save();
 
-    new Answer({
-        answer: "answer1",
-        questionId: 1
-    }).save();
-});
+//     new Answer({
+//         answer: "answer1",
+//         questionId: 1
+//     }).save();
+// });
 
 app.listen(5000);
